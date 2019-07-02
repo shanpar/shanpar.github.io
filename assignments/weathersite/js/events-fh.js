5e1d50a368f796b8c3a3b145d20a5f2fc4ef7097
shanpar <shanpar@byui.edu>
2019-07-02 07:54:35 -0500

    weathersite updates

---
+++ assignments/weathersite/js/events-fh.js
@@ -0,0 +1,29 @@
+var div = document.getElementById("events");
+
+var requestURL = "https://byui-cit230.github.io/weather/data/towndata.json";
+var request = new XMLHttpRequest();
+request.open("GET", requestURL);
+request.responseType = "json";
+request.send();
+request.onload = function() {
+  var data = request.response;
+  showData(data);
+};
+function showData(jsonObj) {
+  var eventData = jsonObj["towns"];
+
+  for (var i = 0; i < eventData.length; i++) {
+    if (eventData[i].name == "Fish Haven") {
+      var myul = document.createElement("ul");
+
+      var events = eventData[i].events;
+      for (var k = 0; k < events.length; k++) {
+        var myli = document.createElement("li");
+        myli.textContent = events[k];
+        myul.appendChild(myli);
+      }
+
+      div.appendChild(myul);
+    }
+  }
+}
